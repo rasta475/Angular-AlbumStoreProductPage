023a6786ee7b206a8d7bb06204fa3c7326f95346
Rob Rastelli <rasta475@gmail.com>
2018-01-15 15:11:56 -0500

    Section 6 changes

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,13 +3,20 @@ import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = "../assets/album.json";
+  private _productUrl = "../assets/products.json";
 
   constructor(private _http: Http) { }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productUrl)
+      .map(response => <Product[]>response.json());
+  }
 
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl)
